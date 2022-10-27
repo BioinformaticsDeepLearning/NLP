d98fb89238b03b2e08739b4df30ccadef944db1a
Geeky Olivia <49845542+BioinformaticsDeepLearning@users.noreply.github.com>
2022-10-27 19:32:14 +0200

    Update Embedding_performace.py

--- Embedding_performace.py
+++ Embedding_performace.py
@@ -1,3 +1,4 @@
+#Import important and necessary libraries#
 from tqdm._tqdm_notebook import tqdm_notebook
 import nltk
 from sklearn.model_selection import train_test_split
@@ -27,7 +28,7 @@ def text_preprocessing(df,col_name):
 df = pd.read_csv("Vig.csv")
 _df_ = text_preprocessing(df,df["Indication", "Drug" ,"Adversereaction"])
 
-#Calculating the word frequency by using nltk#
+#NLTK for Calculating the word frequency#
 words_list = []
 for sentence in cleaned_df.Message:
     words_list.extend(nltk.word_tokenize(sentence))
@@ -38,7 +39,7 @@ freq_dist.most_common(20)
 X_train,X_test, y_train,y_test = train_test_split(_df_, test_size = 0.2,
                                                  ,random_state = 42)
                                                  
-num_words = 10000 # this means 10000 unique words can be taken 
+num_words = 20000 
 tokenizer=Tokenizer(num_words,lower=True)
 df_total = pd.concat([X_train, X_test], axis = 0)
 tokenizer.fit_on_texts(df_total)
@@ -50,12 +51,12 @@ X_test_ = tokenizer.texts_to_sequences(X_test)
 X_test_pad = pad_sequences(X_test_, maxlen = 171, padding = 'post')
                               
 
-# designing our architecture 
-emb_dim = 50 # this means the embedding layer will create  a vector in 100 dimension
+# modeling
+emb_dim = 50 
 model_gensim = Sequential()
-model_gensim.add(Embedding(input_dim = ,# the whole vocabulary size 
-                          output_dim = emb_dim, # vector space dimension
-                          input_length= X_train_pad.shape[1], # max_len of text sequence
+model_gensim.add(Embedding(input_dim = ,
+                          output_dim = emb_dim, 
+                          input_length= X_train_pad.shape[1], 
                           weights = [gensim_weight_matrix],trainable = False))
 model_gensim.add(Dropout(0.2))
 model_gensim.add(Bidirectional(CuDNNLSTM(100,return_sequences=True)))
